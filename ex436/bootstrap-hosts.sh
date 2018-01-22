e3959e6c9409437b431d952365b1a3ced9687b62
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2018-01-22 13:35:16 +0300

    Update bootstrap-hosts.sh

--- ex436/bootstrap-hosts.sh
+++ ex436/bootstrap-hosts.sh
@@ -2,3 +2,4 @@
 echo "192.168.50.10 hosta hosta.example.com" >> /etc/hosts
 echo "192.168.50.11 hostb hostb.example.com" >> /etc/hosts
 echo "192.168.50.12 hostc hostc.example.com" >> /etc/hosts
+echo "192.168.50.1 ha-kvm ha-kvm.example.com" >> /etc/hosts
